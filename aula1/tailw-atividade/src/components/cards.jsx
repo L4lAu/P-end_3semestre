c31a2d54b7a6b6d027ccda5779c767afbba0cddc
LUIS EDUARDO PEREIRA FAVARIS <24250670@educ123.sp.senai.br>
2025-09-02 11:19:41 -0300

    informações postas em flex (lg:)

--- aula1/tailw-atividade/src/components/cards.jsx
+++ aula1/tailw-atividade/src/components/cards.jsx
@@ -1,51 +1,52 @@
 const cards = [
     {
         id: 1,
-        imagem: "lalau",
-        titulo: "titulo",
-        subtitulo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum maxime, porro sed a repellat, omnis culpa fugit illo, odit temporibus placeat dolor adipisci delectus iure autem necessitatibus incidunt sapiente.",
+        imagem: "AI",
+        titulo: "o uso degenerativo da IA, como não deve ser usada",
+        subtitulo: "Aprenda a usar a IA corretamente",
+        tags: "#curso",
     },
     {
         id: 2,
-        imagem: "lalau",
-        titulo: "titulo",
-        subtitulo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum maxime, porro sed a repellat, omnis culpa fugit illo, odit temporibus placeat dolor adipisci delectus iure autem necessitatibus incidunt sapiente.",
+        imagem: "AI",
+        titulo: "como falar da forma correta com a IA",
+        subtitulo: "Aprenda a usar a IA corretamente",
     },
     {
         id: 3,
-        imagem: "lalau",
-        titulo: "titulo",
-        subtitulo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum maxime, porro sed a repellat, omnis culpa fugit illo, odit temporibus placeat dolor adipisci delectus iure autem necessitatibus incidunt sapiente.",
+        imagem: "AI",
+        titulo: "Como programar sua prorpia IA",
+        subtitulo: "Aprenda programar sua IA do 0",
     },
     {
         id: 4,
-        imagem: "lalau",
-        titulo: "titulo",
-        subtitulo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum maxime, porro sed a repellat, omnis culpa fugit illo, odit temporibus placeat dolor adipisci delectus iure autem necessitatibus incidunt sapiente.",
+        imagem: "Phyton",
+        titulo: "Phyton basico",
+        subtitulo: "Aprenda phyton basico",
     },
     {
         id: 5,
-        imagem: "lalau",
-        titulo: "titulo",
-        subtitulo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum maxime, porro sed a repellat, omnis culpa fugit illo, odit temporibus placeat dolor adipisci delectus iure autem necessitatibus incidunt sapiente.",
+        imagem: "Phyton",
+        titulo: "Phyton avançado",
+        subtitulo: "Aprenda phyton do 0 ao Pro",
     },
     {
         id: 6,
-        imagem: "lalau",
-        titulo: "titulo",
-        subtitulo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum maxime, porro sed a repellat, omnis culpa fugit illo, odit temporibus placeat dolor adipisci delectus iure autem necessitatibus incidunt sapiente.",
+        imagem: "Phyton",
+        titulo: "Phyton avançado",
+        subtitulo: "Aprenda phyton do 0 ao Pro",
     },
     {
         id: 7,
-        imagem: "lalau",
-        titulo: "titulo",
-        subtitulo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum maxime, porro sed a repellat, omnis culpa fugit illo, odit temporibus placeat dolor adipisci delectus iure autem necessitatibus incidunt sapiente.",
+        imagem: "Phyton",
+        titulo: "Phyton avançado",
+        subtitulo: "Aprenda phyton do 0 ao Pro",
     },
     {
         id: 8,
-        imagem: "lalau",
-        titulo: "titulo",
-        subtitulo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum maxime, porro sed a repellat, omnis culpa fugit illo, odit temporibus placeat dolor adipisci delectus iure autem necessitatibus incidunt sapiente.",
+        imagem: "Phyton",
+        titulo: "Phyton avançado",
+        subtitulo: "Aprenda phyton do 0 ao Pro",
     },
 ];
 
@@ -53,27 +54,35 @@ const cards = [
 export default function CARDS() {
     return (
         <>
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 m-2 lg:m-10 min-w-90">
-                    {cards.map((card) => (
-                        <div key={card.id} className="bg-white text-black min-w-30 md:min-w-45 lg:max-w-90 max-h-90 md:max-h-100 lg:max-h-110 rounded-2xl">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 m-2 lg:m-10 min-w-90">
+                {cards.map((card) => (
+                    <div key={card.id} className="grid lg:flex  bg-white text-black min-w-30 md:min-w-45 lg:max-w-110 max-h-90 md:max-h-100 lg:max-h-110 rounded-2xl">
+                        <div className="min-w-40">
                             <div className="border border-black m-2 flex justify-center min-h-20 items-center text-2xl font-extrabold text-white bg-gradient-to-br from-green-300 to-black">
                                 {card.imagem}
                             </div>
-                            <div className="font-black text-xl m-2">
+                            <div>
+                                {card.tags}
+                            </div>
+                            
+                        </div>
+
+                        <div>
+                            <div className="font-black text-md m-2">
                                 {card.titulo}
                             </div>
-
                             <div className="text-xs lg:text-md md:text-sm m-2">
                                 {card.subtitulo}
                             </div>
-                            
-                            <div className="m-2 flex justify-end items-end">
+
+                            <div className="m-2 flex justify-end h-auto">
                                 <button className="mx-2 text-white bg-green-600 hover:bg-green-700 cursor-pointer p-2 rounded-md border border-black">acessar</button>
                             </div>
-
                         </div>
-                    ))}
-                </div>
+
+                    </div>
+                ))}
+            </div>
 
         </>
     )
